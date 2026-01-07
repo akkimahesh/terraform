5a88351ba3a481b4818898b16eb12520e17e5254
akkimahesh <maheshakki799@gmail.com>
2026-01-07 17:43:39 +0530

    initial commit

--- conditions/ec2.tf
+++ conditions/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "roboshop" {
-    count = 9
+    count = length(var.instance_names)
     ami           = var.ami_id
-    instance_type = var.instance_names[count.index] == "mongodb" ? "t3.large" : "t3.small"
+    instance_type = var.instance_names[count.index] == "mongodb" || var.instance_names[count.index] == "mysql" ||var.instance_names[count.index] == "shipping" ? "t3.small" : "t2.micro"
     vpc_security_group_ids = [aws_security_group.roboshop_sg.id]
     tags = {
         Name = var.instance_names[count.index]
